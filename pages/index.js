45a4a8e78dfd5b8121203c02468d10ff8b7c72d1
Ted Littledale <ted@unrest.tech>
2021-02-08 01:17:02 +0000

    Update metadata

--- pages/index.js
+++ pages/index.js
@@ -13,15 +13,18 @@ const Home = () => {
   return (
     <>
       <Head>
-        <title>Example Title</title>
+        <title>Ted Littledale</title>
         <meta name="twitter:card" content="summary" />
         <meta name="twitter:creator" content="@_superted" />
-        <meta property="og:url" content="https://countdown.tedspace.me/" />
-        <meta property="og:title" content="Freedom calculator" />
-        <meta property="og:description" content="Example Description" />
+        <meta property="og:url" content="https://tedspace.me/" />
+        <meta property="og:title" content="Ted Littledale" />
+        <meta
+          property="og:description"
+          content="Creative Technologist, Front End developer, Data Visualisation tinkerer, and a successful startup founder."
+        />
         <meta
           property="og:image"
-          content="https://res.cloudinary.com/dc1yk89ve/image/upload/v1611689943/Screenshot_2021-01-26_at_19.38.28_ouuy8y.png"
+          content="https://res.cloudinary.com/dc1yk89ve/image/upload/v1612746964/Screenshot_2021-02-08_at_01.15.25_pfixww.png"
         />
         <link rel="icon" href="animated-favicon.gif" type="image/gif"></link>
         <link
@@ -43,7 +46,7 @@ const Home = () => {
         />
         <link rel="manifest" href="/site.webmanifest" />
         <link rel="mask-icon" href="/safari-pinned-tab.svg" color="#5bbad5" />
-        <meta name="theme-color" content="#ffffff" />
+        <meta name="theme-color" content="#F0476F" />
       </Head>
       <Pages />
     </>
